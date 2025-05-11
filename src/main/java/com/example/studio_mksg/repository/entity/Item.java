8bfd97dcfd0f87e526f81bc4c32d8dfa5e1ec393
momoe_kitazaki <kitazaki.momoe@alhinc.jp>
2025-05-11 18:24:04 +0900

    ItemEntity作成

--- src/main/java/com/example/studio_mksg/repository/entity/Item.java
+++ src/main/java/com/example/studio_mksg/repository/entity/Item.java
@@ -1,4 +1,37 @@
 package com.example.studio_mksg.repository.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
 public class Item {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int price;
+
+    @Column
+    private String image;
+
+    @Column
+    private int stock;
+
+    @Column(name = "created_date", insertable = false, updatable = false)
+    private Date createdDate;
+
+    @Column(name = "updated_date", insertable = false, updatable = true)
+    private Date updatedDate;
+
 }
