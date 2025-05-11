98357a3d63427a803e215f9779ad3f8ae0d42d46
momoe_kitazaki <kitazaki.momoe@alhinc.jp>
2025-05-11 17:27:36 +0900

    初！コミットだよ！

---
+++ src/main/java/com/example/studio_mksg/StudioMksgApplication.java
@@ -0,0 +1,13 @@
+package com.example.studio_mksg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioMksgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioMksgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studio_mksg/StudioMksgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studio_mksg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioMksgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
